fa2c2ee6f7b296bf654ad54c05cd781e2cf30f00
kandlapallibharathkumar <kandlapallibharathkumar@gmail.com>
2026-09-25 23:49:28 +0530

    Time: 18 ms (77.09%) | Memory: 108.2 MB (47.47%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,35 @@
+// import java.util.Arrays;
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
+// this is best for the using hashset 
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+}
